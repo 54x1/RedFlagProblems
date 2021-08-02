12e2787da57d94d53afe6dbea5c73ad671cbe4d3
54x1 <five4x1@gmail.com>
2021-08-03 01:04:13 +1000

    *

--- frontend/RedFlags/bkups/bk/server/game.js
+++
@@ -1,9 +0,0 @@
-const { GRID_SIZE } = require('./constants');
-
-module.exports = {
-  initGame
-}
-
-function initGame() {
-
-}
--- server/constants.js
+++ server/constants.js
@@ -1,19 +1,7 @@
-module.exports = {
-  getVal,
-}
-function getVal(){
-$.getJSON("perks.json", function(data, perkOne, perkTwo){
+const FRAME_RATE = 10;
+const GRID_SIZE = 20;
 
-    var randIn = Math.floor(Math.random() * (data.perks.length));
-    var randIn2 = Math.floor(Math.random() * (data.perks.length));
-      var perkValue1 = data.perks[randIn].card;
-        var perkValue2 = data.perks[randIn2].card;
-        var perkOne = perkValue1;
-          var perkTwo = perkValue2;
-          // console.log(perkOne);
-          alert(perkOne);
-          alert(perkTwo);
-          return perkOne;
-                return perkTwo;
-});
+module.exports = {
+  FRAME_RATE,
+  GRID_SIZE,
 }
