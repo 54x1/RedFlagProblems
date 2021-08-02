0b74dac79b6d0c389f982a03631050414dbef7c6
54x1 <five4x1@gmail.com>
2021-08-03 01:08:39 +1000

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,12 @@
+const BG_COLOUR = '#231f20';
+const SNAKE_COLOUR = '#c2c2c2';
+const FOOD_COLOUR = '#e66916';
+
+// $('.card-section').each(function(){
+//   $(this).on('click', function(){
+//     alert('here');
+//   });
+// });
 $( ".card-section" ).click(function() {
     if ( $( this ).hasClass("ui-draggable-dragging") ) {
 
@@ -19,7 +28,13 @@ $('.card-section').on('touchmove', function(){
 // $(this).css({"left"}) =
 });
 const socket = io('https://flags-54x1.herokuapp.com/');
-
+// var perk1val = document.getElementsByClassName('perk3');
+// var perk2val = document.getElementsByClassName('perk4');
+// // console.log(perk2val.value);
+// socket.emit('perks', {
+//        perk1: perk1val.value,
+//        perk2s: perk2val.value
+//    });
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
 socket.on('gameOver', handleGameOver);
@@ -55,58 +70,90 @@ function newGame() {
   init();
 }
 
+// function joinGame1(perkData1, perkData2){
+//   // socket.emit('newGame1');
+//     $.getJSON("perks.json",function(data){
+//         var randIn = Math.floor(Math.random() * (data.perks.length));
+//         var randIn2 = Math.floor(Math.random() * (data.perks.length));
+//         var perkData1 = (data.perks[randIn].card);
+//         var perkData2 = (data.perks[randIn2].card);
+//         var perks = [perkData1, perkData2];
+// perk(perks);
+//     });
+//
+//
+// }
+function perk(perks) {
+  console.log(perks);
+return perks;
+}
+
+function getPerksData(perks2){
+$('.perk1').append('perks2');
+$('.perk2').append(perkData2);
+}
 function joinGame() {
   const code = gameCodeInput.value;
   socket.emit('joinGame', code);
   init();
 }
 
-let canvas, ctx;
 let playerNumber;
 let gameActive = false;
 
 function init() {
   initialScreen.style.display = "none";
   gameScreen.style.display = "block";
+  //
+  // canvas = document.getElementById('canvas');
+  // ctx = canvas.getContext('2d');
+  //
+  // canvas.width = canvas.height = 600;
+  //
+  // ctx.fillStyle = BG_COLOUR;
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //
+  // document.addEventListener('keydown', keydown);
 
-  canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
+  $.getJSON('flags.json', function(data) {
+    console.log(data);
+    var randInt = Math.floor(Math.random() * (data.flags.length));
+    var randInt2 = Math.floor(Math.random() * (data.flags.length));
+      var randInt3 = Math.floor(Math.random() * (data.flags.length));
+        var randInt4 = Math.floor(Math.random() * (data.flags.length));
+    $('.flags >.card-section:first-child').append(data.flags[randInt].card);
+    $('.flags >.card-section:nth-child(2)').append(data.flags[randInt2].card);
+    $('.flags >.card-section:nth-child(3)').append(data.flags[randInt3].card);
+    $('.flags >.card-section:last-child').append(data.flags[randInt4].card);
+    // $('.card-section>p:nth-child(3)').append(data.flags[randInt2].card);
 
-  canvas.width = canvas.height = 600;
+  });
 
-  ctx.fillStyle = BG_COLOUR;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // $.getJSON("perks.json",function(data){
+  //     var randIn = Math.floor(Math.random() * (data.perks.length + 1));
+  //     var randIn2 = Math.floor(Math.random() * (data.perks.length + 1));
+  //     // getPerks(randIn, randIn2, data);
+  //
+  //
+  //     console.log(data);
+  //     // alert('heree1');
+  //
+  // });
 
-  document.addEventListener('keydown', keydown);
+
+
+  // $.getJSON('perks.json', function(data) {
+  // console.log(data);
+  //
+  // });
+
   gameActive = true;
 }
 
-function keydown(e) {
-  socket.emit('keydown', e.keyCode);
-}
+
 
 function paintGame(state) {
-  ctx.fillStyle = BG_COLOUR;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const food = state.food;
-  const gridsize = state.gridsize;
-  const size = canvas.width / gridsize;
-
-  ctx.fillStyle = FOOD_COLOUR;
-  ctx.fillRect(food.x * size, food.y * size, size, size);
-
-  paintPlayer(state.players[0], size, SNAKE_COLOUR);
-  paintPlayer(state.players[1], size, 'red');
-}
-
-function paintPlayer(playerState, size, colour) {
-  const snake = playerState.snake;
-
-  ctx.fillStyle = colour;
-  for (let cell of snake) {
-    ctx.fillRect(cell.x * size, cell.y * size, size, size);
-  }
 }
 
 function handleInit(number) {
@@ -117,6 +164,8 @@ function handleGameState(gameState) {
   if (!gameActive) {
     return;
   }
+
+
   gameState = JSON.parse(gameState);
   requestAnimationFrame(() => paintGame(gameState));
 }
@@ -130,19 +179,25 @@ function handleGameOver(data) {
   gameActive = false;
 
   if (data.winner === playerNumber) {
-    alert('You Win!');
+    // alert('You Win!');
   } else {
-    alert('You Lose :(');
+    // alert('You Lose :(');
   }
 }
 
 function handleGameCode(gameCode) {
   gameCodeDisplay.innerText = gameCode;
 }
-
+// function handleGameCode1(gameCode1){
+// $.getJSON("perks.json",function(data){
+//     var randIn = Math.floor(Math.random() * (data.perks.length));
+//     var randIn2 = Math.floor(Math.random() * (data.perks.length));
+//     $('body').append('perkData');
+// });
+// }
 function handleUnknownCode() {
   reset();
-  alert('Unknown Game Code')
+  alert('Unknown Game Code');
 }
 
 function handleTooManyPlayers() {
@@ -150,8 +205,10 @@ function handleTooManyPlayers() {
   alert('This game is already in progress');
 }
 
+
 function reset() {
   playerNumber = null;
+  // $('.perk1, .perk2').html('');
   gameCodeInput.value = '';
   initialScreen.style.display = "block";
   gameScreen.style.display = "none";
